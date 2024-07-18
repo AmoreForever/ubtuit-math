b0020bf27f8c9c9375767c2b3bf35601f59a07de
Hikamoru <itz.hikamoru@mail.ru>
2024-07-18 10:36:49 +0500

    fix

--- 0017-chiziqli2.py
+++ 0017-chiziqli2.py
@@ -1,6 +1,7 @@
 from math import *
+
 x, y = map(float, input().split())
 
-f1 = ((2 * tan(x + (pi / 6))) / (1 / 3) + cos(y + x) ** 2) + log(x**2 + 2)
+f1 = (2 * tan(x + pi / 6)) / (1 / 3 + (cos(y + x**2)) ** 2) + log((x**2 + 2), 2)
 
 print(f"{f1:.2f}")
